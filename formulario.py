b51cea2ac084e3e3afbc1fff9f7bb738b00425bc
Dev 1 <dev1.aracati@gmail.com>
2020-08-06 12:43:07 +0000

    Adiciona Metodos

--- formulario.py
+++ formulario.py
@@ -2,3 +2,7 @@ class Formulario:
     def __init__(self):
         self.StopButton = Button()
 	self.PauseButton = Button()
+    def stop_music(self):
+	pass
+    def pause_music(self):
+        pass
